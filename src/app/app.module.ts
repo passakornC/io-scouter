957050d2270a69486eb6d55f4eb1a4587d1cd66d
Passakorn Choosuk (Bee) <passakornchoosuk@gmail.com>
2020-12-05 16:05:39 +0700

    Add http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {LayoutModule} from './layouts/layout.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {LayoutModule} from './layouts/layout.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LayoutModule
+    LayoutModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
